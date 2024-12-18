3e5800b96b90f03bb2509d5708a58cdf0da1ca44
VikumPerera <vikumchathuranga92@gmail.com>
2024-12-18 18:58:33 +0530

    Disable rabbitmq h2 database and enable mysql with dockerization

--- src/main/java/com/microservice/account/entity/Account.java
+++ src/main/java/com/microservice/account/entity/Account.java
@@ -1,6 +1,8 @@
 package com.microservice.account.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
